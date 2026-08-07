44dea873f90270806e843ff3ac38a35e489f3ee0
asimfarooq5 <asim@xconn.io>
2026-08-07 22:23:06 +0500

    auto fetch quic native lib

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -77,3 +77,24 @@ flutter {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
+
+val fetchQuicNativeLib = tasks.register("fetchQuicNativeLib") {
+    val outputFile = file("src/main/jniLibs/arm64-v8a/libdart_quic_ffi.so")
+    outputs.file(outputFile)
+    onlyIf { !outputFile.exists() }
+    doLast {
+        outputFile.parentFile.mkdirs()
+        ant.invokeMethod(
+            "get",
+            mapOf(
+                "src" to "https://github.com/xconnio/xconn-dart/releases/latest/download/libdart_quic_ffi-android-arm64.so",
+                "dest" to outputFile,
+                "verbose" to true,
+            ),
+        )
+    }
+}
+
+tasks.named("preBuild") {
+    dependsOn(fetchQuicNativeLib)
+}
